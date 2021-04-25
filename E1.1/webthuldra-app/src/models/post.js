d18e62e3501686a1334af3f0bd41bd5456d9c696
cegarciav <cegarcia2@uc.cl>
2021-04-25 03:03:05 -0400

    DB set up with users and posts models created

---
+++ E1.1/webthuldra-app/src/models/post.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.user)
+    }
+  };
+  post.init({
+    caption: DataTypes.STRING,
+    media: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'post',
+  });
+  return post;
+};
