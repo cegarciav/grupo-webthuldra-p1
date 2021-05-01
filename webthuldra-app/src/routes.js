9745c89c1a372cf26e2c42668da13234dd4037ea
cegarciav <cegarcia2@uc.cl>
2021-05-01 11:50:10 -0400

    Add global function routes to app

--- webthuldra-app/src/routes.js
+++ webthuldra-app/src/routes.js
@@ -15,7 +15,22 @@ router.use((ctx, next) => {
                 queryOptions.page = +page;
             return ctx.router.url('root', {}, { query: queryOptions });
         },
-        userPath: (id) => id? ctx.router.url('users.show', id): '/'
+        userPath: (id) => id? ctx.router.url('users.show', id): '/',
+        usersPath: (page) => {
+            let queryOptions = {};
+            if (page && page > 1)
+                queryOptions.page = +page;
+            return ctx.router.url('users.list', {}, { query: queryOptions });
+        },
+        newUserPath: () => ctx.router.url('users.new'),
+        updateUserPath: (id) => ctx.router.url('users.update', id),
+        parseDate: (dateString) => {
+            const dateObject = new Date(dateString);
+            const day = dateObject.getDay();
+            const month = dateObject.getMonth();
+            const year = dateObject.getFullYear();
+            return `${(day<10)?('0' + day): day}-${(month<10)?('0' + month): month}-${year}`;
+        }
     };
     return next();
 });
